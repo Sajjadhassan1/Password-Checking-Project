4759a46cf35f36f590e55c2df3d9aaa3f6c94bd7
𝒮𝒶𝒿𝒿𝒶𝒹 𝓗𝓪𝓼𝓼𝓪𝓷 <sajjadhassan12345ss@gmail.com>
2026-03-26 19:24:33 +0300

    Add files via upload

---
+++ Password.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+    string password;
+    int score = 0;
+
+    cout << "Enter your password: ";
+    cin >> password;
+
+    if(password.length() >= 8)
+        score++;
+
+    bool hasUpper = false;
+    bool hasLower = false;
+    bool hasDigit = false;
+    bool hasSymbol = false;
+
+    for(char c : password){
+
+        if(isupper(c))
+            hasUpper = true;
+
+        else if(islower(c))
+            hasLower = true;
+
+        else if(isdigit(c))
+            hasDigit = true;
+
+        else
+            hasSymbol = true;
+    }
+
+    if(hasUpper) score++;
+    if(hasLower) score++;
+    if(hasDigit) score++;
+    if(hasSymbol) score++;
+
+    cout << "Password Strength: ";
+
+    if(score <= 2)
+        cout << "Weak";
+
+    else if(score <= 4)
+        cout << "Medium";
+
+    else
+        cout << "Strong";
+
+    return 0;
+}
